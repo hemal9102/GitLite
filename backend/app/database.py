2d0cf47da63d6f6d87368d5068b81a4f4049e7c7
Hemal Shah <hemal.shah2004@gmail.com>
2025-12-23 21:44:12 +0530

    render update

--- backend/app/database.py
+++ backend/app/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 import os
 
-DATABASE_URL = os.getenv("DATABASE_URL", "postgresql+psycopg2://${POSTGRES_USER}:${POSTGRES_PASSWORD}@localhost:${POSTGRES_PORT}/${POSTGRES_DB}")
+DATABASE_URL = os.getenv("DATABASE_URL", "${DATABASE_URL}")
 
 engine = create_engine(DATABASE_URL, pool_pre_ping=True)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
